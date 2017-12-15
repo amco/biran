a672ba8fb8799a8c93d721dd57fbdb15fe759444
Brian Lanier <brianl@amco.me>
2017-12-15 11:57:59 -0800

    Adding tests... kind of
    - Documenting what should be tested is kind of the same thing right?

--- test/biran_test.rb
+++ test/biran_test.rb
@@ -4,4 +4,10 @@ class Biran::Test < ActiveSupport::TestCase
   test "truth" do
     assert_kind_of Module, Biran
   end
+
+  test "extension for config tasks should contain period" do
+    # assert extension passed with period('.conf') matches '.conf'
+    # assert extension passed without perios('conf') matches '.conf'
+    skip
+  end
 end
