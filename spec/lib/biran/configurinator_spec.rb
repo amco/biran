864fada5a32b9bf17ab052a70e6fe36f3062bd85
Brian Lanier <brianl@amco.me>
2022-08-25 11:14:41 -0700

    Update tests (woot!!)
    - Update some spec describe and it statements to better say what is
    being tested
    - Organize a few tests under a specific method that is being tested
    - Add a few tests for aliases and nested values
    - No idea what I am doing... so better check this

--- spec/lib/biran/configurinator_spec.rb
+++ spec/lib/biran/configurinator_spec.rb
@@ -4,20 +4,32 @@ require "biran"
 describe Biran::Configurinator do
   subject { described_class.new }
 
-  it "test" do
-    expect(subject.files_to_generate).to eql({my_config: {extension: ".yml"}})
+  describe "files_to_generate" do
+    it "will generate my_config.yml" do
+      expect(subject.files_to_generate).to eql({my_config: {extension: ".yml"}})
+    end
   end
 
-  describe "psych version" do
+  describe "process_config_file" do
     let(:data_content) { ERB.new(File.read("config/app_config.yml")).result }
     let(:hash_content) { { "development" => {}, "test" => {} } }
+
+    it "will handle aliases in yaml" do
+      expect(subject.config[:my_first_value]).to eq(11)
+      expect(subject.config[:my_second_value]).to eq(2)
+    end
+
+    it "will handle nested aliases in yaml" do
+      expect(subject.config[:nested_values][:my_third_value]).to eq(13)
+      expect(subject.config[:nested_values][:my_fourth_value]).to eq(4)
+    end
 
     context "when psych version is < 4.0" do
       before(:example) do
         stub_const("Psych::VERSION", "3.2")
       end
 
-      it "calls right method" do
+      it "safe_laod should get correct arguments" do
         expect(YAML).to receive(:safe_load).with(data_content, [], [], true).and_return(hash_content)
         subject.files_to_generate
       end
@@ -28,7 +40,7 @@ describe Biran::Configurinator do
         stub_const("Psych::VERSION", "4.0")
       end
 
-      it "calls right method" do
+      it "safe_laod should get correct arguments" do
         expect(YAML).to receive(:safe_load).with(data_content, aliases: true).and_return(hash_content)
         subject.files_to_generate
       end
